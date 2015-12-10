6583339c5e8f0b7e4a7e37c76add9f41617bbc79
GULPF <oscarnihlgard@gmail.com>
2015-12-10 23:34:12 +0100

    Fixes

--- src/Sensors/I28Data.java
+++ src/Sensors/I28Data.java
@@ -6,15 +6,15 @@ public class I28Data implements SensorData
 {
 	public final int currentDoseRate;
 	public final int peakDoseRate;
-	public final int accumulatedDose;
+	public final int accumulatedDoseRate;
 	public final int errorCode;
 	
 	public I28Data(JSONObject lastReceivedData)
 	{
 		JSONObject data = lastReceivedData.getJSONObject("Data");
-		currentDoseRate = data.getInt("currentDoseRate");
-		peakDoseRate = data.getInt("peakDoseRate");
-		accumulatedDose = data.getInt("AccumulatedDose");
+		currentDoseRate = data.getInt("CurrentDoseRate");
+		peakDoseRate = data.getInt("PeakDoseRate");
+		accumulatedDoseRate = data.getInt("AccumulatedDoseRate");
 		
 		JSONObject state = lastReceivedData.getJSONObject("State");
 		errorCode = state.getInt("ErrorCode");
